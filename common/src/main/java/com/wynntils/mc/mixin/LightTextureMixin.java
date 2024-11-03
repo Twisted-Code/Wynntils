49f487c2c8f7098858670e443ccbf13362b59dfe
Jamie <JamCal117@gmail.com>
2024-11-03 15:29:53 +0000

    wip: Port LightmapEvent

--- common/src/main/java/com/wynntils/mc/mixin/LightTextureMixin.java
+++ common/src/main/java/com/wynntils/mc/mixin/LightTextureMixin.java
@@ -4,23 +4,18 @@
  */
 package com.wynntils.mc.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.blaze3d.platform.NativeImage;
-import com.wynntils.core.events.MixinHelper;
-import com.wynntils.mc.event.LightmapEvent;
 import net.minecraft.client.renderer.LightTexture;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LightTexture.class)
 public class LightTextureMixin {
-    @WrapOperation(
-            method = "updateLightTexture",
-            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;setPixelRGBA(III)V"))
-    private void updateLightmapRGB(NativeImage image, int x, int y, int rgb, Operation<Void> original) {
-        final LightmapEvent lightmapEvent = new LightmapEvent(rgb);
-        MixinHelper.post(lightmapEvent);
-        original.call(image, x, y, lightmapEvent.getRgb());
-    }
+    // FIXME: Port to 1.21.3
+    //    @WrapOperation(
+    //            method = "updateLightTexture",
+    //            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;setPixelRGBA(III)V"))
+    //    private void updateLightmapRGB(NativeImage image, int x, int y, int rgb, Operation<Void> original) {
+    //        final LightmapEvent lightmapEvent = new LightmapEvent(rgb);
+    //        MixinHelper.post(lightmapEvent);
+    //        original.call(image, x, y, lightmapEvent.getRgb());
+    //    }
 }
